a70e9105542742f868f985009b7940233fe8136b
Khanza Fadila Azhara <khanzafadilaazz@gmail.com>
2023-05-28 21:34:02 +0700

    delete file

--- tes.js
+++
@@ -1,9 +0,0 @@
-// Tes koneksi database
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Database connection has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
