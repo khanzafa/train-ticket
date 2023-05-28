4296852fa165e5ea06589ab485aedc3d9d9909c4
Khanza Fadila Azhara <khanzafadilaazz@gmail.com>
2023-05-28 21:27:30 +0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const router = express.Router();
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+
+router.use(session({
+  secret:'v654h6cv5oi2435xuu',
+  resave:false,
+  saveUninitialized:false
+}));
+
+/* Tampilan Beranda */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* Tampilan Login */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+/* Proses Login */
+router.post('/login', async(req, res) => {
+  try {
+    const {username, password} = req.body;    
+    const User = req.app.get('User');
+    const user = await User.findOne({where:{username}});
+    
+    //User not found
+    if(!user){
+      return res.status(404).json({message:'User tidak ditemukan'});
+    }    
+    bcrypt.compare(password, user.password, (err, result)=>{
+      if(err){
+        console.error('Error comparing passwords:', err);
+        return res.status(500).json({message: 'Error comparing passwords'});
+      }
+      if(result){
+        //Login berhasil
+        req.session.user = user;
+        return res.status(200).json({message:'Login berhasil'});
+      }else{
+        //Invalid password      
+        return res.status(401).json({message:'Password tidak cocok'});        
+      }
+    });
+  } catch(error){
+    console.error('Kesalahan saat login:', error);
+    return res.status(500).json({message:'Kesalahan server'});
+  }
+});
+
+/* Tampilan Register */
+router.get('/register', function(req, res, next){
+  res.render('register');
+});
+/* Proses Register */
+router.post('/register', async (req, res)=>{
+  const {name, username, password, email, phone} = req.body;
+  try {
+    const User = req.app.get('User');
+    const newUser = await User.create({
+      name, username, password, email, phone
+    });
+    
+    res.redirect('/login');
+    return res.status(200).json({message: 'Registrasi berhasil!'});    
+  }catch(error){
+    console.error('Kesalahan pembuatan user:', error);
+    return res.status(500).json({message: 'Kesalahan pembuatan user'});
+  }
+});
+
+/* Proses Logout */
+router.get('/logout', function(req, res, next) {
+  req.session.destroy();
+  res.redirect('/login');
+});
+
+router.get('/search', function (req, res){
+  res.render('search');
+});
+router.post('/search', async (req, res) => {
+
+})
+
+module.exports = router;
---
+++ tes.js
@@ -0,0 +1,9 @@
+// Tes koneksi database
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
